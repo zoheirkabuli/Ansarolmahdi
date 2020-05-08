fdab386012b6200942df8cdd0d8626ac7cb12648
maryam <maryam@gmail.com>
2020-05-08 16:58:56 +0430

    add two Classes

--- app/src/main/java/com/example/ansarolmahdi/Classes.java
+++ app/src/main/java/com/example/ansarolmahdi/Classes.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.github.clans.fab.FloatingActionButton;
 
 public class Classes extends AppCompatActivity {
+
+    private FloatingActionButton fabStudent,fabClass,fabTeacher;
 
 
 
@@ -16,8 +19,40 @@ public class Classes extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_classes);
 
+        init();
 
+        fabTeacher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toast("Teacher");
+            }
+        });
+
+        fabStudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toast("Student");
+            }
+        });
+
+        fabClass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toast("Class");
+            }
+        });
 
     }
 
+    private void init() {
+        fabClass = findViewById(R.id.fab_class);
+        fabStudent = findViewById(R.id.fab_student);
+        fabTeacher = findViewById(R.id.fab_teacher);
+    }
+
+
+
+    public void toast(String txt){
+        Toast.makeText(this, txt, Toast.LENGTH_SHORT).show();
+    }
 }
