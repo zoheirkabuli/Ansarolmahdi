02c378e9f9844336686f2df817bbcdbf6a4711a1
parigol <parigol.h2017@gmail.com>
2020-05-11 19:53:45 +0430

    add info dialog.xml

--- app/src/main/java/com/example/ansarolmahdi/AddTeacher.java
+++ app/src/main/java/com/example/ansarolmahdi/AddTeacher.java
@@ -7,8 +7,10 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -44,16 +46,25 @@ public class AddTeacher extends AppCompatActivity {
 
     private void runAlert() {
 
+//        new AlertDialog.Builder(this)
+//                .setTitle("Select")
+//                .setCancelable(false)
+//                .setMultiChoiceItems(items,null,null)
+//                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int i) {
+//                        Toast.makeText(AddTeacher.this, "Yes Button clicked", Toast.LENGTH_SHORT).show();
+//                    }
+//                })
+//                .create()
+//                .show();
+
+        ViewGroup viewGroup = findViewById(android.R.id.content);
+
         new AlertDialog.Builder(this)
-                .setTitle("Select")
-                .setCancelable(false)
-                .setMultiChoiceItems(items,null,null)
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Toast.makeText(AddTeacher.this, "Yes Button clicked", Toast.LENGTH_SHORT).show();
-                    }
-                })
+                .setView(LayoutInflater.from(this).inflate(R.layout.info_dialog
+                        ,viewGroup
+                        ,false))
                 .create()
                 .show();
     }
